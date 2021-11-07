630c7b57de8ca3f94914555f29d3e3b965d76918
IoanMica <92159342+ioanmica@users.noreply.github.com>
2021-11-07 21:47:02 +0200

    Update nslookup.py

--- nslookup.py
+++ nslookup.py
@@ -1,5 +1,13 @@
 import subprocess
+from django.db import connection
 
 domain = input("Enter the Domain: ")
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 print(output)
+
+
+def find_user(username):
+    with connection.cursor() as cur:
+        cur.execute(f"""select username from USERS where name = '%s'""" % username)
+        output = cur.fetchone()
+    return output
