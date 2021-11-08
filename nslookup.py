3bf0de6b7220aac5df81820c0a1acc93fcac9229
IoanMica <92159342+ioanmica@users.noreply.github.com>
2021-11-08 23:31:13 +0200

    Update nslookup.py

--- nslookup.py
+++ nslookup.py
@@ -5,6 +5,7 @@ domain = input("Enter the Domain: ")
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 print(output)
 
+smth = "see you later"
 
 def find_user(username):
     with connection.cursor() as cur:
